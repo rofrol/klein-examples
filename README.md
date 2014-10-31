fbf785931f6ac017084a5fbae798cfe3e709ce5e
Roman Frołow <rofrol@gmail.com>
2014-10-31 18:41:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+cd some_example
+composer install
+php -S localhost:8001
+```
+
+Open browser at http://localhost:8001
---
+++ hello-world/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$klein = new \Klein\Klein();
+
+$klein->respond('GET', '/hello-world', function () {
+    return 'Hello World!';
+});
+
+$klein->dispatch();
+?>
